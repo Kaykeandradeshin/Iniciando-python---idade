99135ea8058ebfc9909b0916bddabd4a96c23b6c
kayke <2kaykede168@gmail.com>
2024-03-21 16:06:38 -0300

    primeira versão

---
+++ programa de idade 01.py
@@ -0,0 +1,9 @@
+
+print('ola kayke tudo bem, por favor respnda as perguntas para calcularmos sua idades')
+
+nasci = int(input('digite o ano que você nasceu:'))
+atual = int(input('digite o ano atual:'))
+
+result = atual - nasci
+
+print(f'você tem ou tera em alguns meses {result} anos')
