98988e1b99a927f801a2704c582ec94c6874bbf2
kayke <2kaykede168@gmail.com>
2024-03-22 14:44:20 -0300

    versão 2

--- programa de idade 01.py
+++ programa de idade 01.py
@@ -1,9 +1,22 @@
 
-print('ola kayke tudo bem, por favor respnda as perguntas para calcularmos sua idades')
+import time 
+
+print('Olá, Seja Bem Vindo!!!')
+time.sleep(2)
+
+print('Por favor responda as perguntas para calcularmos sua idade')
 
 nasci = int(input('digite o ano que você nasceu:'))
 atual = int(input('digite o ano atual:'))
+mes = int(input('digite o mês que você nasceu:'))
 
 result = atual - nasci
 
-print(f'você tem ou tera em alguns meses {result} anos')
+if mes >= 4:
+    result = atual - nasci - 1
+
+else:
+    result = atual - nasci
+
+
+print(f'você tem {result} anos')
